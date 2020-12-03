cf2cf706d065aa8a583c9c6e6a42544b015ffdd4
willem_vermin <willem_vermin@79837d8a-40e5-47fd-869e-05d31bbd7fe9>
2020-12-03 12:28:51 +0000

    devel version 3.1.8~pre2 see ChangeLog
    git-svn-id: https://svn.code.sf.net/p/xsnow/code/trunk@379 79837d8a-40e5-47fd-869e-05d31bbd7fe9

--- xsnow-devel/src/version.h
+++ xsnow-devel/src/version.h
@@ -19,7 +19,7 @@
 #-# 
 */
 #pragma once
-#define VERSION "3.1.8~pre1"
+#define VERSION "3.1.8~pre2"
 #define VERSIONBY \
    "December 14th 2001 by Rick Jansen \n" \
    "December 2020 by Willem Vermin"
